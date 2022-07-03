27070fdbb02d5d6519f0090e834e362e80fe9932
ToobaAli12 <toobaali266@gmail.com>
2022-07-03 19:35:28 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>home</title>
+	<!--swipe css link-->
+	<link
+  rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+/>
+	<!--cdn awesome font links -->
+		<link rel="stylesheet" type="text/css" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+		<!--coustome file-->
+     <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+ <!--header section start-->
+ <section calss="header">
+ 	<a href="home.php" class="logo"> travel </a>
+ 	<nav class="navbar">
+ 		<a href="HOME.PHP">HOME</a>
+ 		<a href="ABOUT.PHP">ABOUT</a>
+ 		<a href="PACKAGE.PHP">PACKAGE</a>
+ 		<a href="BOOK.PHP">BOOK </a>
+ 	</nav>
+ 	
+ </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<!-- coustome js file link-->
+<script src="js/scr.js"></script>
+</body>
+</html>
